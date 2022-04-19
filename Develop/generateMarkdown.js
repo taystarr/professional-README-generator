a0ccd3fc3eb059665af6ca0729eb3fb3d5cf044c
Tay Starr <tstarrroberts@gmail.com>
2022-04-19 12:25:01 -0400

    add optional license section and description

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -13,19 +13,60 @@ const renderLicenseBadge = license => {
   }
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseDescription = license => {
+  if (!license) {
+    return '';
+  } else if (license === 'Apache') {
+    return `## License
+  This project is under Apache license. Apache is a type of permissive free software license, it allows the user to do nearly anything they want with the code, with very few exceptions. Given that the Apache License text is quite dense, to further read up on the legality please click the license badge at the top of the page.
+  `;
+  } else if (license === 'MIT') {
+    return `## License
+  This project is under MIT license. MIT is a type of permissive free software license, it is the leanest license and basically means that users can do whatever they want with the application.
+  `;
+  } else if (license === 'GPL') {
+    return `## License
+  This project is under GPL license. GPL is the heaviest open source license. This license prevent users from using work in unfair way, any copy or modification of the original code must also be released under same license.
+  `;
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ## ${renderLicenseBadge(data.license)}
-`;
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage 
+  ${data.usage}
+
+  ${renderLicenseDescription(data.license)}
+
+  ## Contributing
+  ${data.contribute}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  For addational information please click here ${data.name} to see my GitHub repositories or email me at ${data.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# README Generator
-  ## [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+# khrgkjdhldf
+  ## [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  fbszdlkzfd
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  nsfkjnbfc
+
+  ## Usage 
+  kjbfbnknf
+
+  ## License
+  This project is under MIT license. MIT is a type of permissive free software license, it is the leanest license and basically means that users can do whatever they want with the application.
+  
+
+  ## Contributing
+  nkjfvx
+
+  ## Tests
+  nfdvklnd
+
+  ## Questions
+  For addational information please click here lksdbndfg to see my GitHub repositories or email me at nklfndkjfzdjdzfz.
+  
