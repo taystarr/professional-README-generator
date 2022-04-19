712197fe14808f5d1d81641e585c8e76f5c12948
Tay Starr <tstarrroberts@gmail.com>
2022-04-19 07:43:05 -0400

    validation, title and license add

--- Develop/generateMarkdown.js
+++ Develop/generateMarkdown.js
@@ -1,13 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+// License link included in function
 const renderLicenseBadge = license => {
   if (!license) {
     return '';
-  } else if (data.license === 'Apache') {
+  } else if (license === 'Apache') {
     return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-  } else if (data.license === 'MIT') {
+  } else if (license === 'MIT') {
     return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-  } else if (data.license === 'GPL') {
+  } else if (license === 'GPL') {
     return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
   }
 };
@@ -21,10 +22,9 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-const generateMarkdown = data => {
-  // const { license, title, name, email, test, contribute, usage, install, description } = projectInfo;
-  return `# Project Title
-
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ## ${renderLicenseBadge(data.license)}
 `;
 }
 
--- index.js
+++ index.js
@@ -10,62 +10,138 @@ const questions = () => {
             // Project title
             type: 'input',
             name: 'title',
-            message: 'What is the title of your project?'
+            message: 'What is the title of your project?',
             // add validate to all prompts below
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project title!');
+                    return false;
+                }
+            }
         },
         {
-            // Project description
+            // Enter github username for questions section
             type: 'input',
-            name: 'description',
-            message: 'Provide a description of your project'
+            name: 'name',
+            message: 'Enter your GitHub username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                }
+            }
         },
         {
-            // Installation instructions
-            type: 'input',
-            name: 'install',
-            message: 'Provide installation instructions for user'
-        },
-        {
-            // Usage information
-            type: 'input',
-            name: 'usage',
-            message: 'Provide instructions for use'
+            type: 'confirm',
+            name: 'licenseConfirm',
+            message: 'Would you like to add a license for this project?',
+            default: false
         },
         {
             // Lisence from List
             type: 'list',
             name: 'license',
             message: 'Choose the appropriate license from the following choices',
-            choices: ['MIT', 'Apache', 'GPL']
+            choices: ['MIT', 'Apache', 'GPL'],
+            when: ({ licenseConfirm }) => {
+                if (licenseConfirm) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
             // https://gist.github.com/nicolasdao/a7adda51f2f185e8d2700e1573d8a633#1-mit
             // follow link for license description
+        },
+        {
+            // Project description
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            // Installation instructions
+            type: 'input',
+            name: 'install',
+            message: 'Provide installation instructions for user',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter installation instructions!');
+                    return false;
+                }
+            }
+        },
+        {
+            // Usage information
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions for use',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter usage instructions!');
+                    return false;
+                }
+            }
         },
         {
             // Contribution guidelines
             type: 'input',
             name: 'contribute',
-            message: 'Provide guidelines on how others can contribute'
+            message: 'Provide guidelines on how others can contribute',
+            validate: contInput => {
+                if (contInput) {
+                    return true;
+                } else {
+                    console.log('Please enter contribution guidelines!');
+                    return false;
+                }
+            }
         },
         {
             // Test instructions
             type: 'input',
             name: 'test',
-            message: 'Provide test instructions for application'
-        },
-        {
-            // Enter github username for questions section
-            type: 'input',
-            name: 'name',
-            message: 'Enter your GitHub username'
+            message: 'Provide test instructions for application',
+            validate: testInput => {
+                if (testInput) {
+                    return true;
+                } else {
+                    console.log('Please enter test instructions!');
+                    return false;
+                }
+            }
         },
         {
             // Enter email address for Questions section
             type: 'input',
             name: 'email',
-            message: 'Enter your email address'
+            message: 'Enter your email address',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email!');
+                    return false;
+                }
+            }
         }
-    ])
-    .then(answers => console.log(answers))
+    ]);
 };
 
 
